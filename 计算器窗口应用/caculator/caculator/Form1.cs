7f48f92ac1fbf7edff1c1158de373acde5f70970
unknown <252943600@qq.com>
2020-02-27 22:24:34 +0800

    first commit

---
+++ 计算器窗口应用/caculator/caculator/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(t1.Text);
+            double b = Convert.ToDouble(t3.Text);
+            if (t2.Text == "/" && t3.Text == "0")
+            {
+                t4.Text = "除零错误";
+                return;
+            }
+
+           
+            switch (t2.Text)
+            {
+                case "+":
+                    t4.Text = $"{a + b}";
+                    break;
+                case "-":
+                    t4.Text = $"{a - b}";
+                    break;
+                case "*":
+                    t4.Text = $"{a * b}";
+                    break;
+                case "/":
+                    t4.Text = $"{a / b}";
+                    break;
+            }
+        }
+    }
+}
